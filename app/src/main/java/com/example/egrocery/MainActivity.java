aab49b2309c9280206871d14066e5749c214e47e
jai <jhirani114@rku.ac.in>
2020-02-24 17:41:36 +0530

    updated by jai

--- app/src/main/java/com/example/egrocery/MainActivity.java
+++ app/src/main/java/com/example/egrocery/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private Button joinNowButton,loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowButton =findViewById(R.id.main_join_now_btn);
+        loginButton= findViewById(R.id.main_login_btn);
     }
 }
